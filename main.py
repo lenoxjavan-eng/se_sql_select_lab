a98aa95ab52aec364bd17e64bcddfb4daa3fa851
Lenox Javan <lenox.javan@student.moringaschool.com>
2026-07-16 10:38:13 +0300

    Complete initial SQL select steps

--- main.py
+++ main.py
@@ -1,18 +1,26 @@
 # STEP 1A
 # Import SQL Library and Pandas
+import sqlite3
+import pandas as pd
 
 # STEP 1B
 # Connect to the database
-conn = None
+conn = sqlite3.connect("data.sqlite")
 
 
 # STEP 2
 # Replace None with your code
-df_first_five = None
+df_first_five = pd.read_sql("""
+SELECT employeeNumber, lastName
+FROM employees
+""", conn)
 
 # STEP 3
 # Replace None with your code
-df_five_reverse = None
+df_five_reverse = pd.read_sql("""
+SELECT lastName, employeeNumber
+FROM employees
+""", conn)
 
 # STEP 4
 # Replace None with your code
